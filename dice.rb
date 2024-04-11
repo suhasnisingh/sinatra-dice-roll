692a874f3fbf64c3c99a269c1cca96a78eceee1e
suhasnisingh <165236685+suhasnisingh@users.noreply.github.com>
2024-04-11 16:50:04 +0000

    complete

--- dice.rb
+++ dice.rb
@@ -2,9 +2,16 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -13,4 +20,61 @@ end
 
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  erb(:two_six)
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  sum = first_die
+	
+  @outcome = "You rolled a #{first_die}."
+	
+  erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
+  sum = @rolls.sum
+	
+  @outcome = "You rolled a #{@rolls[0]}, a #{@rolls[1]}, a #{@rolls[2]}, a #{@rolls[3]}, and a #{@rolls[4]} for a total of #{sum}."
+	
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
